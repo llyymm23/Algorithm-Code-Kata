b1a6140dc9718a9474f6058068ce72c7b8b3edc6
llyymm23 <154869093+llyymm23@users.noreply.github.com>
2024-06-10 21:55:09 +0900

    [Bronze V] Title: A+B, Time: 112 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,7 @@
+require('fs').readFileSync('dev/stdin').toString();
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+
+console.log(a+b);
