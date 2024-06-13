81240eea59ad4cfbfb2907807d5da656253b2fb5
llyymm23 <154869093+llyymm23@users.noreply.github.com>
2024-06-13 13:44:27 +0900

    [Bronze V] Title: 윤년, Time: 132 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString();
+
+const year = parseInt(input);
+
+if (year % 4 == 0) {
+    if (year % 100 !== 0 || year % 400 == 0) {
+        console.log("1");
+    } else {
+        console.log("0");
+    }
+} else {
+    console.log("0");
+}
