d98825e0dba9096961c47097234c6672271e2791
llyymm23 <154869093+llyymm23@users.noreply.github.com>
2024-06-13 14:55:37 +0900

    [Bronze V] Title: 구구단, Time: 116 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString();
+
+const num = parseInt(input);
+
+for (let i = 1; i < 10; i++) {
+    console.log(`${num} * ${i} = ${num * i}`);
+}
