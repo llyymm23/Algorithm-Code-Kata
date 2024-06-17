9ad0e19a16b803ddee7841a8ba5c50a02cf176b6
llyymm23 <154869093+llyymm23@users.noreply.github.com>
2024-06-17 09:39:26 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 9572 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -1,11 +1,8 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-let i=0;
-while(i<input.length-1){
-    let nums = input[i].split(' ').map(x => Number(x));
-    
-    a = nums[0];
-    b = nums[1];
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
+for(let i=0;i<input.length;i++){
+    const a = parseInt(input[i].split(' ')[0]);
+    const b = parseInt(input[i].split(' ')[1]);
     console.log(a+b);
-    i++;
-}
+};
