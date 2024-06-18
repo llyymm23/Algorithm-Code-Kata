4983d189849e0d54c71dd7c19660246bf8487f7a
llyymm23 <154869093+llyymm23@users.noreply.github.com>
2024-06-19 08:23:34 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -3,14 +3,7 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 const arr = input.map(Number);
 const max = Math.max(...arr);
-let count = 0;
-
-for(let i=0;i<arr.length;i++){
-    count++;
-    if(arr[i]===max){
-        break;
-    }
-}
+const count = arr.indexOf(max);
 
 console.log(max);
-console.log(count);
+console.log(count+1);
