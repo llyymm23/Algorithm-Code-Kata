7004161d83e97501d17e19a45108c512d0428e1e
llyymm23 <154869093+llyymm23@users.noreply.github.com>
2024-06-18 09:32:37 +0900

    [Bronze V] Title: 개수 세기, Time: 116 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.js
+++ 백준/Bronze/10807. 개수 세기/개수 세기.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-const [n, arr, v] = input;
+const arr = input[1].split(' ').map(Number);
 
-const result = arr.split(' ').filter(num => num === v).length;
+const result = arr.filter(num => num === Number(input[2])).length;
 console.log(result);
