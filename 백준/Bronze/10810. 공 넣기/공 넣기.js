cbfdf0524ca97973f8e7b62f3cc9af3093e46b45
llyymm23 <154869093+llyymm23@users.noreply.github.com>
2024-06-19 08:57:37 +0900

    [Bronze III] Title: 공 넣기, Time: 112 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.js
+++ 백준/Bronze/10810. 공 넣기/공 넣기.js
@@ -1,17 +1,15 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-const [N, M] = input[0].split(" ").map(el => +el);
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let arr = new Array(N);
+const [n, m] = input[0].split(' ').map(Number);
 
-for(let i = 1; i <= M; i++ ){
-   let [start,end,k] = input[i].split(" ").map(el => +el);
-    for(start; start <= end; start++){
-        arr[start-1] = k;
-    } 
+let arr = new Array(n).fill(0);
+
+for (let a = 1; a <= m; a++) {
+    let [i, j, k] = input[a].split(' ').map(Number);
+    for (let b = i; b <= j; b++) {
+        arr[b - 1] = k;
+    }
 }
 
-for(let j=0; j<arr.length; j++){
-    typeof arr[j] === "undefined" ? arr[j] = 0 : arr[j]
-}
-
-console.log(arr.join(" "));
+console.log(arr.join(' '));
