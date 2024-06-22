661114c2ede858f2770a3c175d8e184f4bc40c8a
llyymm23 <154869093+llyymm23@users.noreply.github.com>
2024-06-22 11:06:40 +0900

    [Bronze V] Title: 문자와 문자열, Time: 100 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-const str = input[0].split('');
+const str = input[0];
 const n = parseInt(input[1]);
 
 console.log(str[n - 1]);
