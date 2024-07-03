e42f5952f7ca89ce4e25b1cc8c25339de69e5154
llyymm23 <154869093+llyymm23@users.noreply.github.com>
2024-07-03 17:12:38 +0900

    [Bronze II] Title: 문자열 반복, Time: 112 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const input = fs.readFileSync('dev/stdin').toString().split('\n');
 
-const n = Number(input[0]);
+const num = Number(input[0]);
+
 let result = '';
 
-for (let i = 1; i <= n; i++) {
-  const count = Number(input[i].split(' ')[0]);  
-  const str = input[i].split(' ')[1];  
-  
-  for (let j = 0; j < str.length; j++) {
-    for (let k = 0; k < count; k++) {
-      result += str[j];
-    }
-  }
-  result += '\n';
-}
+for (let i = 1; i <= num; i++) {
+  const [count, str] = input[i].split(' ');
 
+  for (let j = 0; j < str.length; j++) {
+    result += str.charAt(j).repeat(Number(count));
+  }
+    
+  result += `\n`;
+}
 console.log(result);
